84a8310d6df9ce823e3fd90509e4a7ff19a9e0a5
Claudia <claudiatcruz2002@gmail.com>
2022-10-10 18:16:20 +0200

    comentarios Medicion

--- app/src/main/java/ctorcru/upv/sprint0android/Modelo/Medicion.java
+++ app/src/main/java/ctorcru/upv/sprint0android/Modelo/Medicion.java
@@ -2,41 +2,60 @@ package ctorcru.upv.sprint0android.Modelo;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
+// ---------------------------------------------------------------------------------------------
+/**
+ * @brief Esta clase se encarga de montar el objeto que se subirá a la base de datos
+ * Autora: Claudia Torres Cruz
+ * Archivo: Medicion.java
+ **/
+// ---------------------------------------------------------------------------------------------
 public class Medicion {
-    String medida;
-    String fecha;
-
+    // Atributos
+    private String medida;
+    private String fecha;
+    // ---------------------------------------------------------------------------------------------
+    /**
+     * @brief Constructor de la clase
+     * @param medida
+     * @return objeto Medicion
+     * Diseño: int --> Medicion() --> Medicion
+     **/
+    // ---------------------------------------------------------------------------------------------
     public Medicion(int medida) {
         this.medida =String.valueOf(medida);
         this.fecha = fechaDB();
-    }
-
+    } //()
+    // ---------------------------------------------------------------------------------------------
+    /**
+     * @brief Con este método se obtiene la medida
+     * @return medida
+     * Diseño: --> getMedida() --> String
+     **/
+    // ---------------------------------------------------------------------------------------------
     public String getMedida() {
         return medida;
-    }
-
-    public void setMedida(String medida) {
-        this.medida = medida;
-    }
-
+    } //()
+    // ---------------------------------------------------------------------------------------------
+    /**
+     * @brief Con este método se obtiene la fecha
+     * @return fecha
+     * Diseño: --> getFecha() --> String
+     **/
+    // ---------------------------------------------------------------------------------------------
     public String getFecha() {
         return fecha;
-    }
-
+    } //()
+    // ---------------------------------------------------------------------------------------------
+    /**
+     * @brief Con este método se establece la fecha a la hora de crear el objeto
+     * @return fechaActual
+     * Diseño: --> fechaDB() --> String
+     **/
+    // ---------------------------------------------------------------------------------------------
     private String fechaDB(){
         Date fecha = new Date();
         SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         String fechaActual = formato.format(fecha);
         return fechaActual;
-    }
-    public String toJSON(){
-        String res = "{" +
-                "\"id\":\""+"0"+"\"," +
-                "\"valor\":\""+this.medida+"\","+
-                "\"fecha\":\""+this.fecha+"\""+
-                "}";
-        return res;
-    }
-
-}
+    } //()
+} //()
